1eb54a5899a3cf80c3fd11b5f7bebb8a5828ec82
Rob <72252737+rdawley@users.noreply.github.com>
2022-12-22 17:14:07 +0000

    tidy up

--- main.tf
+++ main.tf
@@ -31,15 +31,14 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
+  
   name    = "blog_new"
-
-  vpc_id = data.aws_vpc.default.id
+  vpc_id  = data.aws_vpc.default.id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 }
